33166a4533d9f7449a26a0b3091c1d4d74dc34ff
sofiia987657 <sofiiakotik256@gmail.com>
2025-03-10 16:43:10 +0100

    Add files via upload

---
+++ tarefa4.py
@@ -0,0 +1,6 @@
+resp = input("Como te chamas?")
+print("Guau... que nome mais intetesante" , resp)
+print("Ti es unha grande persoa" , resp);
+print("Vales moito" , resp);
+print("Non hai ninguen mais elegantr ca " , resp);
+print("e..." , resp , ", o mundo adorate!");
