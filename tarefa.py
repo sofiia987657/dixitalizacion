e73a2a92df948740b6de2eada087c296dcd5b6fa
sofiia987657 <sofiiakotik256@gmail.com>
2025-03-10 16:24:31 +0100

    Add files via upload

---
+++ tarefa.py
@@ -0,0 +1,13 @@
+print("*******Compra por internet***************")
+
+reposta = input("Cantas ducias de ovos queres  ")
+respostaNum = int(reposta)
+
+ovos = 12*respostaNum
+
+print("Tes que comprar ", ovos, "este numero de ovos")
+
+print("**********************")
+
+    
+
