1f73dca3dfc54480f7085d6c5e5f95f8da6898b0
sofiia987657 <sofiiakotik256@gmail.com>
2025-03-18 13:57:46 +0100

    Add files via upload

---
+++ famosetes.py
@@ -0,0 +1,10 @@
+print("Un xogo")
+deporte = input("Cual es tu deporte favorito?")
+if:(deporte == "atletismo") :
+    pais = input("En cual pais preferieres vivir?")
+    if(pais == "Estadoa Unidos") :
+        altura = input ("Preferias ser alta?")
+if:(altura =="si"):
+print("Eres Sera Williams la exjugadora de tenis estadounidadense ganadora 
+elif( deporte =="gimnasta")
+pais input ("")
